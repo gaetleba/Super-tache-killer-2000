9513be572b62382b98fd5b29d880fe11790f8ba4
Warrows <warrows@gamer666.fr>
2014-11-29 16:42:52 +0100

    First commit

---
+++ src/fr/vikingGameJam/tacheKiller2000/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.vikingGameJam.tacheKiller2000.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.vikingGameJam.tacheKiller2000.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
