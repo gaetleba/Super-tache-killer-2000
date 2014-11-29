b489bffb809abc496239daac4d9f38770a9d1083
Warrows <warrows@gamer666.fr>
2014-11-29 17:42:35 +0100

    Added moustache

--- src/fr/vikingGameJam/tacheKiller2000/Game.java
+++ src/fr/vikingGameJam/tacheKiller2000/Game.java
@@ -13,12 +13,12 @@ public class Game extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("assets/moustache.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
