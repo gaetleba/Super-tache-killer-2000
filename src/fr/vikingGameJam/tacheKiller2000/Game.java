2efbe3ce0d5a0141b20692979e2d5df6d6ca6d6c
Warrows <warrows@gamer666.fr>
2014-11-29 18:38:53 +0100

    Blinking mustache

--- src/fr/vikingGameJam/tacheKiller2000/Game.java
+++ src/fr/vikingGameJam/tacheKiller2000/Game.java
@@ -4,24 +4,46 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Game extends ApplicationAdapter {
+public class Game extends ApplicationAdapter
+{
 	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("assets/moustache.png");
-	}
+	Texture moustacheTexture;
+	private float stateTime;
 
 	@Override
-	public void render () {
+	public void create()
+	{
+		batch = new SpriteBatch();
+		moustacheTexture = new Texture("assets/moustache.png");
+		stateTime = 0;
+
+		{
+			Sprite[] moustacheFrames = new Sprite[6];
+			int width = 1024;
+			int height = 64;
+			int nbFrames = 6;
+			for (int i = 0; i < nbFrames; i++)
+				moustacheFrames[i] = new Sprite(moustacheTexture, i * width/nbFrames, 0,
+						width/nbFrames, height);
+			moustache = new Animation(0.2F, moustacheFrames);
+			moustache.setPlayMode(Animation.PlayMode.LOOP);
+		}
+	}
+
+	private Animation moustache;
+
+	@Override
+	public void render()
+	{
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(moustache.getKeyFrame(stateTime/100), 0, 64);
 		batch.end();
+		stateTime ++;
 	}
 }
