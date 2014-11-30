01a8589869b8074a12699bb26f96655ae54d8df7
Warrows <warrows@gamer666.fr>
2014-11-30 01:28:12 +0100

    Added gameover animation class

---
+++ src/fr/vikingGameJam/tacheKiller2000/GameOverAnimation.java
@@ -0,0 +1,34 @@
+package fr.vikingGameJam.tacheKiller2000;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class GameOverAnimation extends Animation
+{
+	private static GameOverAnimation instance = null;
+	public static final int WIDTH = 512;
+	public static final int HEIGHT = 256;
+
+	public static GameOverAnimation getInstance()
+	{
+		if (instance == null)
+		{
+			int nbFrames = 2;
+			Sprite[] frames = new Sprite[nbFrames];
+			for (int i = 0; i < nbFrames; i++)
+				frames[i] = new Sprite(
+						new Texture("assets/game_over.png"), i * WIDTH, 0, WIDTH,
+						HEIGHT);
+			instance = new GameOverAnimation(2.0F, frames);
+			instance.setPlayMode(PlayMode.LOOP_RANDOM);
+		}
+		return instance;
+	}
+	
+	private GameOverAnimation(float frameDuration, TextureRegion[] keyFrames)
+	{
+		super(frameDuration, keyFrames);
+	}
+}
